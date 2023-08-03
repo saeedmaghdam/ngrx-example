e3c1cc5b4bcc3ed8e76d9d13344c5e99a34d80ec
saeedmaghdam <smasafat@gmail.com>
2023-08-03 09:51:50 +0200

    stopped exporting initialState + refactored reducers

--- src/store/reducers/advancedCounter.reducer.ts
+++ src/store/reducers/advancedCounter.reducer.ts
@@ -2,11 +2,11 @@ import { createReducer, on } from "@ngrx/store";
 import { CounterInterface } from "../interfaces/CounterInterface";
 import * as actions from "../actions/advancedCounter.actions"
 
-export const advancedCounterInitialState: CounterInterface = {
+const initialState: CounterInterface = {
     count: 0
 };
 
-export const advancedCounterReducer = createReducer(advancedCounterInitialState, 
+export const advancedCounterReducer = createReducer(initialState, 
     on(actions.increment, (state: CounterInterface, {steps}) => ({...state, count: state.count + steps})),
     on(actions.decrement, (state: CounterInterface, {steps}) => ({...state, count: state.count - steps})),
     on(actions.reset, (state: CounterInterface) => ({...state, count: 0}))
--- src/store/reducers/counter.reducers.ts
+++ src/store/reducers/counter.reducers.ts
@@ -2,11 +2,11 @@ import { createReducer, on } from "@ngrx/store";
 import { CounterInterface } from "../interfaces/CounterInterface";
 import * as actions from "../actions/counter.actions"
 
-export const counterInitialState: CounterInterface = {
+const initialState: CounterInterface = {
     count: 0
 };
 
-export const counterReducer = createReducer(counterInitialState, 
+export const counterReducer = createReducer(initialState, 
     on(actions.increment, (state: CounterInterface) => ({...state, count: state.count + 1})),
     on(actions.decrement, (state: CounterInterface) => ({...state, count: state.count - 1})),
     on(actions.reset, (state) => ({...state, count: 0}))
